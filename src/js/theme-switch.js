2c590994850839919023bb987a51f701194aacc2
unknown <74738757+leonid-br@users.noreply.github.com>
2021-06-05 13:04:40 +0300

    Правки

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -2,19 +2,29 @@ const refs = {
   checkEl: document.querySelector('.theme-switch__toggle'),
   body: document.querySelector('body'),
 };
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 const CHECKBOX_KEY = 'checkedClick';
 
 function updateView() {
-  refs.checkEl.checked = localStorage.getItem(CHECKBOX_KEY);
+  localStorage.getItem(CHECKBOX_KEY) === Theme.DARK?
+  refs.checkEl.checked = true:
+  refs.checkEl.checked = false;
+  
 }
+
 
 function onChecked() {
   if (refs.checkEl.checked) {
+    refs.body.classList.remove('ligth-theme');
     refs.body.classList.add('dark-theme');
-    localStorage.setItem(CHECKBOX_KEY, refs.checkEl.checked);
+    localStorage.setItem(CHECKBOX_KEY, Theme.DARK);
   } else {
     refs.body.classList.remove('dark-theme');
-    localStorage.removeItem(CHECKBOX_KEY);
+    refs.body.classList.add('light-theme');
+    localStorage.setItem(CHECKBOX_KEY, Theme.LIGHT);
   }
 }
 
